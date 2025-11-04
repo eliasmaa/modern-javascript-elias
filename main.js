a844527aa1d127f391ac64535047f29234112edd
Elias <Eliasmaardal@gmail.com>
2025-11-04 14:27:33 +0100

    solution oppgave1

---
+++ main.js
@@ -0,0 +1,13 @@
+/* oppgavesett-1 oppgave 1*/
+console.log("hello world!")
+/* oppgavesett-1 oppgave 2*/
+console.log("What is up")
+
+/* oppgavesett-1 oppgave 3
+Sting contatication: når flere string blir til en string ved bruk av pluss. kan bli brukt med numbers. Det som skjer normalt når man bruker pluss med bare tall så plusses det sammen.
+Når du tar med en string med tekst så merker js at tallene ikke skal bli plusset men isteden for skrevet ved siden av hverandre. */
+
+/* oppgavesett-1 oppgave 4
+console.log(“Geir Hilmersen er født i året” + 1979 + 42 + “ år gammel!”);
+dette blir GeirHilmersenerfødtiåret197942årgammel!. Dette er fordi når nummeret blir satt sammen med en string som i dette tilfelle er "Geir hilmersen født, og år gammel" også skjer string conatication. 
+derfor plusses ikke tallene sammen, og når string contacitation blir brukt mister man alle mellomrom.*/
