f625442685c4cddacbec55fd2873cc49a72bb964
Elias <Eliasmaardal@gmail.com>
2025-11-11 13:16:55 +0100

    ferdig oppg2b

---
+++ main.js
@@ -0,0 +1,54 @@
+//oppgave-sett2b//
+
+
+ 
+
+//oppg1//
+
+console.log(typeof 42) // number
+
+console.log(typeof "Hei") // string
+
+console.log(typeof true) // boolean
+
+console.log(typeof undefined) // primativ
+
+console.log(typeof null) // object
+
+console.log(typeof Symbol("id")) // symbol
+
+console.log(typeof 12345678901234567890n) // bigint
+
+console.log(typeof {navn: "Ola", alder: 16}) // object  
+
+//oppg2//
+
+
+
+
+// a) undefined. Dette skjer fordi i har blitt deklarert, men ikke initiallisert
+
+
+
+
+
+
+// b) Nå blir i number fordi den nå har blitt deklarert og innitialisert og den har fått en verdi av en tall. 
+
+
+
+
+
+// c) Nå endret i seg fordi den fikk en ny verdi i innitialiseringen. Da endret den seg til en string som kan bli gjenkjent med bokstaver imellom ""
+
+
+let i;
+i = true;
+
+
+console.log(typeof i);
+
+
+
+// d) nå fikk vi bollean. Dette er en datatype som enten sier at noe er usant eller sant
+
