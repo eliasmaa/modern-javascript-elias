313be19b2aaedbee0188f47c81e6e8ea9a445a58
Elias <Eliasmaardal@gmail.com>
2025-11-18 12:21:42 +0100

    ferdig 4

---
+++ 4a.js
@@ -0,0 +1,49 @@
+//oppg 1
+console.log(1 === 1) //true 
+
+console.log(1 === 2) // false
+
+console.log(3==3) // true
+
+console.log(2==3) //false
+
+
+//oppg.2
+
+console.log(1 == "1") //true
+
+console.log(2 === "2") //false
+
+
+//Forklar forskjellen på en sammenligning som bruker tre likhetstegn, og en sammenligning som bruker to likhetstegn (i javascript
+// to likhetstegn ser på verdien av de to variablene på hver sin side, imens tre likhets tegn ser på datatypen
+
+//oppg.3
+console.log(1==1 && 2==2) //true
+
+console.log(1==="1" && 2===2)//false
+
+console.log(1===1 && "2" ===2) //false
+
+console.log(1=="1" && 2=="2") //true
+
+//oppg.4
+console.log(1==="1" || 2===2) //true
+
+ console.log(2=="2" || 1==="1") //true 
+
+ console.log(2===2 || 1===1 || 3==="3") //true
+
+ //oppg 5
+
+ console.log(1 !== 1) //false
+
+ console.log(2 !== 1) //true
+
+ console.log(2 !== 1 && 1 === 1) //true
+
+ console.log(2 !== "2" && 1=== "1" || 2=="2" )  //true
+
+ //oppg6
+
+ 
