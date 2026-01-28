aaa35f620f86c5616d090e379d5e8524097daede
Elias <Eliasmaardal@gmail.com>
2026-01-28 09:59:16 +0100

    ferdig med 7erene

---
+++ 71b.js
@@ -0,0 +1,73 @@
+//task1
+const array=[10, 20, 30]
+
+console.log(array[1])
+
+//task2
+
+let task2=[]
+
+task2[0]="hello";
+task2[1]="world";
+
+console.log(task2)
+//Task3
+const myarray=[1,2,3,4,5]
+
+console.log(myarray.length)
+
+//task4
+let mat=["apple", "banana", "cherry"]
+
+console.log(mat)
+
+mat[1]="blueberry"
+
+console.log(mat)
+
+//task5
+
+let thisarray=[5,10,15]
+
+console.log(thisarray)
+
+thisarray[3]=20
+
+console.log(thisarray)
+
+//task6
+
+let farge=["red","green","blue"]
+console.log(farge[2])
+
+//task7
+ 
+{
+    let størelse=[7,14,21]
+        if(størelse[0]< størelse[2]){
+            console.log(true)
+        }
+        else{
+            console.log(false)
+        }
+}
+
+//task 8
+
+let nr8=[2,4,6,8]
+console.log(nr8[0] + nr8[2])
+
+//task9
+
+nr9=[[1, 2, 3], [4, 5, 6]]
+console.log(nr9[1][1])
+
+//task10
+
+let x =100
+let y =200
+let z =300
+
+let nr10=[x, y, z];
+
+console.log(nr10);
